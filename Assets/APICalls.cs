c384c7a7e416f8ea4dda4f123caec63499dd0e15
dead-papaya <oleg.aki19@gmail.com>
2024-11-17 23:07:52 +0300

    register async await + form

--- Assets/APICalls.cs
+++ Assets/APICalls.cs
@@ -11,38 +11,37 @@ using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
 
-public class TestAPICalls : MonoBehaviour
+public class APICalls : MonoBehaviour
 {
-    private const string url_players = "https://2025.nti-gamedev.ru/api/games/7317031d-266a-41b0-a311-c16c6e3b974a/players/";
-    private const string url_logs = "https://2025.nti-gamedev.ru/api/games/7317031d-266a-41b0-a311-c16c6e3b974a/logs/";
-    private const string filePath = "Assets/save.json";
-
-    [Header("REGISTER")] [SerializeField] private TMP_InputField register_nickname_inputField;
-    [SerializeField] private TMP_InputField register_password_inputField;
-    [SerializeField] private Button register_registerButton;
-    [SerializeField] private TextMeshProUGUI register_errorLog_inputField;
-
-    [Header("LOGIN")] [SerializeField] private TMP_InputField login_nickname_inputField;
-    [SerializeField] private TMP_InputField login_password_inputField;
-    [SerializeField] private Button login_registerButton;
-    [SerializeField] private TextMeshProUGUI login_errorLog_inputField;
-
-
+    public static APICalls Instance { get; private set; }
+    
+    public const string url_players = "https://2025.nti-gamedev.ru/api/games/7317031d-266a-41b0-a311-c16c6e3b974a/players/";
+    public const string url_logs = "https://2025.nti-gamedev.ru/api/games/7317031d-266a-41b0-a311-c16c6e3b974a/logs/";
+    public const string filePath = "Assets/save.json";
+    
     public Account _player = new Account();
 
-    private void Start()
+    private void Awake()
     {
-        register_registerButton.onClick.AddListener(delegate { Register(); });
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Debug.LogError("More than one APICalls Instance!");
+            Destroy(gameObject);
+        }
     }
 
-    private void SaveToJson()
+    public void SaveToJson()
     {
         string json = JsonUtility.ToJson(_player, true); // true для форматирования
         File.WriteAllText(filePath, json);
         Debug.Log($"Данные записаны в {filePath}");
     }
 
-    private void LoadFromJson()
+    public void LoadFromJson()
     {
         if (File.Exists(filePath))
         {
@@ -56,58 +55,7 @@ public class TestAPICalls : MonoBehaviour
         }
     }
 
-    private async UniTask Register()
-    {
-        string nickname = register_nickname_inputField.text;
-        string password = register_password_inputField.text;
-
-        _player.name = nickname;
-        _player.password = password;
-        
-        //<test 
-        Resource t = new Resource();
-        t.name = "JAR";
-        t.count = 19;
-        print($"{t}: {t.name}, {t.count}");
-        _player.resources.Add(t);
-        //>
-        
-        SaveToJson();
-
-        string jsonData = File.ReadAllText(filePath);
-
-        UnityWebRequest request = new UnityWebRequest(url_players, "POST");
-        byte[] jsonToSend = new System.Text.UTF8Encoding().GetBytes(jsonData);
-        request.SetRequestHeader("Authorization", "7317031d-266a-41b0-a311-c16c6e3b974a");
-        // Устанавливаем тело запроса
-        request.uploadHandler = new UploadHandlerRaw(jsonToSend);
-        request.downloadHandler = new DownloadHandlerBuffer();
-        request.SetRequestHeader("Content-Type", "application/json");
-
-        // Отправляем запрос
-        await request.SendWebRequest();
-        
-        if (request.result == UnityWebRequest.Result.Success)
-        {
-            Debug.Log("Запрос успешно отправлен, аккаунт создан: " + request.downloadHandler.text); 
-            await GetAccount(_player.name);
-            
-            LogInfo registerLog = new LogInfo("Account registration: ", _player.name, 
-                new List<Resource>());
-            await SendLogs(registerLog);
-
-            await GetLogs(_player.name);
-        }
-        else
-        {
-            Debug.LogError("Ошибка: " + request.error);
-            if (request.error.Contains("400"))
-            {
-                register_errorLog_inputField.text = "Пользователь с таким именем уже существует.";
-            }
-        }
-    }
-
+    
     public async UniTask SendLogs(LogInfo log)
     {
         string data = JsonUtility.ToJson(log);
@@ -181,22 +129,6 @@ public class TestAPICalls : MonoBehaviour
     }
 }
 
-
-[Serializable]
-public class Account
-{
-    public string name;
-    public string password;
-    public List<Resource> resources;
-
-}
-
-[Serializable]
-public class Resource
-{
-    public string name;
-    public int count;
-}
 
 [Serializable]
 public class LogInfo
