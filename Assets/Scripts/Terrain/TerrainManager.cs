16bf4570a547352ef2d32f4b9023aef2d7b942ab
dead-papaya <oleg.aki19@gmail.com>
2024-11-22 21:01:53 +0300

    decor + resources generation

--- Assets/Scripts/Terrain/TerrainManager.cs
+++ Assets/Scripts/Terrain/TerrainManager.cs
@@ -7,11 +7,21 @@ public class TerrainManager : MonoBehaviour
     public int height = 100; // Высота карты
     public float mapScale = 20f;
     public int seed = 0;
+    [Header("Tiles")]
+    [Header("Ground")]
     public GameObject grassPrefab;  // Префаб для проходимой земли
-    public GameObject waterPrefab;   // Префаб для непроходимой земли
+    public GameObject waterPrefab;   // Префаб для непроходимой земли\
+    [Header("Decorations")]
+    public GameObject[] decorPrefabs; 
+    
+    [Header("Resources")]
+    public GameObject[] resourcePrefabs; 
+    
     public GridGraph gridGraph;     // Ссылка на граф для Pathfinding
     public Transform parentStatic;
     public Transform parentObstacle;
+    public Transform parentDecor;
+    public Transform parentResource;
     
     public int c;
 
@@ -35,15 +45,26 @@ public class TerrainManager : MonoBehaviour
                 // Генерация объектов (если нужно, для визуализации)
                 GameObject tilePrefab = new GameObject();
                 Transform parent = parentStatic;
-                if (sample <= 0.2f)
+                if (sample <= 0.2f) // Generate water
                 {
                     tilePrefab = waterPrefab;
                     parent = parentObstacle;
                 }
-                else if (sample is > 0.2f and <= 1f)
+                else if (sample is > 0.2f and <= 1f) // Generate Grass
                 {
                     tilePrefab = grassPrefab;
                     parent = parentStatic;
+                    if (Random.value is > 0.0009f and <= 0.01f)
+                    {
+                        GameObject spawnedDecor = Instantiate(decorPrefabs[Random.Range(0, decorPrefabs.Length)], new Vector3(x/4f, y/4f, 0) - new Vector3(width/8f, height/8f), Quaternion.identity);
+                        spawnedDecor.transform.parent = parentDecor;
+                    }
+                    else if (Random.value < 0.0009f)
+                    {
+                        GameObject spawnedResource = Instantiate(resourcePrefabs[Random.Range(0, resourcePrefabs.Length)], 
+                            new Vector3(x/4f, y/4f, 0) - new Vector3(width/8f, height/8f), Quaternion.identity);
+                        spawnedResource.transform.parent = parentResource;
+                    }
                 }
                 
                 GameObject spawnedBlock = Instantiate(tilePrefab, new Vector3(x/4f, y/4f, 0) - new Vector3(width/8f, height/8f), Quaternion.identity);
@@ -57,15 +78,15 @@ public class TerrainManager : MonoBehaviour
     {
         gridGraph = AstarPath.active.data.gridGraph;
 
-        // Инициализация клеток для навигации
-        for (int x = 0; x < width; x++)
-        {
-            for (int y = 0; y < height; y++)
-            {
-                // Получаем узел на основе мировых координат
-                GridNode node = (GridNode)gridGraph.GetNode(x, y);
-            }
-        }
+        // // Инициализация клеток для навигации
+        // for (int x = 0; x < width; x++)
+        // {
+        //     for (int y = 0; y < height; y++)
+        //     {
+        //         // Получаем узел на основе мировых координат
+        //         GridNode node = (GridNode)gridGraph.GetNode(x, y);
+        //     }
+        // }
 
         // Перестроим граф пути
         AstarPath.active.Scan();
